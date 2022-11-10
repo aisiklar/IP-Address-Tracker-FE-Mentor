41be25e8e61c5c79e56953a5673a494a87215da5
alptekin isiklar <alptekin@192.168.1.10>
2022-11-11 02:41:26 +0300

    working on the infobox comp. API fetching is done. TO DO NEXT: classify the userinput, whether it is a domain name or an ip address. Then form the correct url and path accordingly

--- src/components/info-box/InfoBox.component.jsx
+++ src/components/info-box/InfoBox.component.jsx
@@ -1,13 +1,34 @@
 import "./InfoBox.styles.css";
 import FetchedData from "./FetchedData.component";
-import API_KEY from "../../.secret";
+//import API_KEY from "../../.secret";
 
 const InfoBox = (props) => {
+  console.log("props.data: ", props.data);
+  //console.log(' env. variable: ',process.env.REACT_APP_IP_API_KEY);
 
-  console.log('props.data: ', props.data);
-  console.log('env. variable: ',process.env.REACT_APP_IP_API_KEY);
-  //console.log('API_KEY: ', API_KEY);
-  
+  const data = props.data;
+  const apiKey = process.env.REACT_APP_IP_API_KEY;
+  const baseUrl = "https://geo.ipify.org/api/v2/country?apiKey=" + apiKey;
+  //  console.log("queryData: ", queryData);
+
+  const getIpData = async () => {
+    
+    // classify the incoming "data", whether it is an ip address or a domain name...
+
+    let queryData = baseUrl + data;
+    
+    try {
+      const res = await fetch(queryData);
+      const returnData = await res.json();
+
+      await console.log("response from the ip API: ", returnData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getIpData();
+
   return (
     <div className="infobox-data-container">
       <FetchedData text="IP ADDRESS" data="IP Address"></FetchedData>
