3a9257c51ae48417e8d713f3652c06a79d49faa8
alptekin isiklar <alptekin@192.168.1.10>
2022-11-12 09:50:06 +0300

    worked on infobox comp. Fetching api data and displaying is OK. Work in process: the classifying of user input and the map api connection.

--- src/components/info-box/InfoBox.component.jsx
+++ src/components/info-box/InfoBox.component.jsx
@@ -1,40 +1,77 @@
+import { useState } from "react";
 import "./InfoBox.styles.css";
 import FetchedData from "./FetchedData.component";
 //import API_KEY from "../../.secret";
 
 const InfoBox = (props) => {
+  const [ipAddress, setIpAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [timezone, setTimezone] = useState("");
+  const [isp, setIsp] = useState("");
+
+  //console.log('ipAddress, city, timezone, isp: ', ipAddress, city, timezone, isp);
+
   console.log("props.data: ", props.data);
   //console.log(' env. variable: ',process.env.REACT_APP_IP_API_KEY);
 
-  const data = props.data;
+  let data = props.data;
+  console.log('data and the type: ', data, typeof(data));
   const apiKey = process.env.REACT_APP_IP_API_KEY;
-  const baseUrl = "https://geo.ipify.org/api/v2/country?apiKey=" + apiKey;
-  //  console.log("queryData: ", queryData);
+  let inputTypeParam = '';
+  const baseUrl =
+    "https://geo.ipify.org/api/v2/country,city,vpn?apiKey=" + apiKey;
+
+  // domain sample: google.com
+  // ip sample: 8.8.8.8
+  // classify whether the input data is domain name or ip Address
+  const findInputType = (data) => {
+    if (data.includes('@')) {
+      console.log('input data type: email')
+    }
+
+    if (data.matchAll(/[a-z]/gi).length >= 1) {
+      console.log(data.matchAll(/[a-z]/gi))
+      console.log('input data type: domain')
+    }
+
+    if (data.match(/[0-9], ./) >= 1) {
+      console.log('input data type: ip address')
+    }
+
+    return inputTypeParam
+  };
+
+  if (data !== '') {
+    console.log('whether the user input data is not empty string')
+    findInputType(data);
+  }
 
   const getIpData = async () => {
-    
-    // classify the incoming "data", whether it is an ip address or a domain name...
 
-    let queryData = baseUrl + data;
-    
+    //findInputType(data);
+    let queryData = baseUrl + inputTypeParam + data;
+
     try {
       const res = await fetch(queryData);
       const returnData = await res.json();
-
-      await console.log("response from the ip API: ", returnData);
+      //await console.log("response from the ip API: ", returnData);
+      await setIpAddress(returnData.ip);
+      await setCity(returnData.location.city);
+      await setTimezone(returnData.location.timezone);
+      await setIsp(returnData.isp);
     } catch (error) {
       console.log(error);
     }
   };
 
-  getIpData();
+  //getIpData();
 
   return (
     <div className="infobox-data-container">
-      <FetchedData text="IP ADDRESS" data="IP Address"></FetchedData>
-      <FetchedData text="LOCATION" data="NY"></FetchedData>
-      <FetchedData text="TIMEZONE" data="timezone"></FetchedData>
-      <FetchedData text="ISP" data="ISP"></FetchedData>
+      <FetchedData text="IP ADDRESS" data={ipAddress}></FetchedData>
+      <FetchedData text="LOCATION" data={city}></FetchedData>
+      <FetchedData text="TIMEZONE" data={timezone}></FetchedData>
+      <FetchedData text="ISP" data={isp}></FetchedData>
     </div>
   );
 };
