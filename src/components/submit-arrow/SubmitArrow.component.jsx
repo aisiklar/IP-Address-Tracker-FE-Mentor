6eae2b6927bcbb1f707b208f6cb36a86ab34e3c0
alptekin isiklar <alptekin@192.168.1.10>
2022-11-22 00:21:24 +0300

    arrow styling done. merge with main

--- src/components/submit-arrow/SubmitArrow.component.jsx
+++ src/components/submit-arrow/SubmitArrow.component.jsx
@@ -1,25 +1,24 @@
 import "./SubmitArrow.styles.css";
 import { useState } from "react";
-import arrow from '../../images2/icon-arrow.svg';
+import arrow from "../../images2/icon-arrow.svg";
 
 const SubmitArrow = (props) => {
   //console.log('props.ifClicked: ', props.ifClicked);
- 
-//  const [arrowClicked, setArrowClicked] = useState(false);
+
+  //  const [arrowClicked, setArrowClicked] = useState(false);
 
   const submitHandler = (event) => {
-    console.log('button clicked');
-    console.log('event: ', event);
+    console.log("button clicked");
+    console.log("event: ", event);
     props.ifClicked(true);
-  }
-  
+  };
+
   return (
     <div className="arrow-container">
-      <input
-        className="arrow"
-        alt="button"
-        onClick={submitHandler}
-      ></input>
+      <button>
+        {" "}
+        <img src={arrow} alt="arrow" onClick={submitHandler}></img>
+      </button>
     </div>
   );
 };
